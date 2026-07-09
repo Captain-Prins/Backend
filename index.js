856f02ebe2de6fdbd645588f552c06a0fefe2c7b
Jeff <jepre.dinglasan03@gmail.com>
2026-07-09 16:46:53 +0800

    learning the Node JS

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 import http from "node:http";
-const users = [
-  {
-    name: "John Doe",
-    id: 1,
-    role: "Student",
-  },
-  {
-    name: "joseph Prince",
-    id: 2,
-    role: "Student",
-  },
-  {
-    name: "Christian Santos",
-    id: 3,
-    role: "Teacher",
-  },
-];
+import "dotenv/config";
+import { readFile, writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function readUsersFromFile() {
+  const data = await readFile(join(__dirname, "data", "users.json"), "utf-8");
+  return JSON.parse(data);
+}
+
+async function saveUser(users) {
+  await writeFile(
+    join(__dirname, "data", "users.json"),
+    JSON.stringify(users, null, 2),
+  );
+}
+function sendJSON(res, statuscode, data) {
+  res.writeHead(statuscode, { "Content-type": "application/json" });
+  res.end(JSON.stringify(data));
+}
+function sendHTML(res, statuscode, html) {
+  res.writeHead(statuscode, { "Content-type": "text/HTML" });
+  res.end(html);
+}
 //creating the server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   console.log(req.method, req.url);
 
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -25,14 +35,15 @@ const server = http.createServer((req, res) => {
 
   //Handle GET /api/users (list or filter)
   if (req.method === "GET" && url.pathname === "/api/users") {
+    const users = await readUsersFromFile();
     const role = url.searchParams.get("role");
+
     let filteredUsers = users;
 
     if (role) {
       filteredUsers = users.filter((user) => user.role === role);
     }
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(filteredUsers));
+    sendJSON(res, 200, filteredUsers);
   }
 
   //Hanndle POST /api/users
@@ -43,26 +54,34 @@ const server = http.createServer((req, res) => {
       body += chunk.toString();
     });
 
-    req.on("end", () => {
+    req.on("end", async () => {
       try {
         const newUserData = JSON.parse(body);
 
+        if (!newUserData.name || !newUserData.role) {
+          sendJSON(res, 400, { error: "name and role are required fields" });
+          return;
+        }
+
+        const users = await readUsersFromFile();
+
+        const newId =
+          users.length > 0 ? Math.max(...users.map((user) => user.id)) + 1 : 1;
+
         const newUser = {
           name: newUserData.name,
-          id: users.length + 1,
+          id: newId,
           role: newUserData.role,
         };
 
-        if (!newUser.name || !newUser.role) {
-          res.writeHead(400, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({ error: "name and role are required fields" }),
-          );
-        }
-
         users.push(newUser);
 
-        res.writeHead(201, { "content-type": "application/json" });
+        await saveUser(users);
+
+        sendJSON(res, 201, {
+          message: "User Created Successfully",
+          user: newUser,
+        });
 
         res.end(
           JSON.stringify({
@@ -72,18 +91,12 @@ const server = http.createServer((req, res) => {
         );
         console.log(users);
       } catch (error) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        sendJSON(res, 400, { error: "Invalid JSON data" });
       }
 
-      req.on("error", (err)=>{
-        res.writeHead(500, {"content-type":"application/json"});
-        res.end(JSON.stringify({error:"internal server error"}));
-      })
-
-
-
-
+      req.on("error", (err) => {
+        sendJSON(res, 500, { error: "Invalid JSON data" });
+      });
     });
   }
 
@@ -102,21 +115,19 @@ const server = http.createServer((req, res) => {
       body += chunk.toString();
     });
 
-    req.on("end", () => {
+    req.on("end", async () => {
       try {
         const updatedUserData = JSON.parse(body);
 
-        if (!newUser.name || !newUser.role) {
-          res.writeHead(400, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({ error: "name and role are required fields" }),
-          );
+        if (!updatedUserData.name || !updatedUserData.role) {
+          sendJSON(res, 400, { error: "name and role are required fields" });
         }
+        const users = await readUsersFromFile();
+
         const userIndex = users.findIndex((user) => user.id === userId);
 
         if (userIndex === -1) {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ error: "User not found" }));
+          sendJSON(res, 404, { error: "User not found" });
           return;
         }
 
@@ -125,19 +136,15 @@ const server = http.createServer((req, res) => {
           name: updatedUserData.name,
           role: updatedUserData.role,
         };
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            message: "User Updated Successfully",
-            user: users[userIndex],
-          }),
-        );
+        await saveUser(users);
+        sendJSON(res, 200, {
+          message: "User Updated Successfully",
+          user: users[userIndex],
+        });
 
         console.log(users);
       } catch (error) {
-        res.writeHead(400, { "content-type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        sendJSON(res, 400, { error: "Invalid JSON data" });
       }
     });
   }
@@ -149,23 +156,19 @@ const server = http.createServer((req, res) => {
     parthPart[3]
   ) {
     const userId = parseInt(parthPart[3]);
-
+    const users = await readUsersFromFile();
     const userIndex = users.findIndex((user) => user.id === userId);
     if (userIndex === -1) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "User not found" }));
+      sendJSON(res, 404, { error: "User not found" });
       return;
     }
 
     const deletedUser = users.splice(userIndex, 1)[0];
-
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(
-      JSON.stringify({
-        message: "User Deleted Successfully",
-        user: deletedUser,
-      }),
-    );
+    await saveUser(users);
+    sendJSON(res, 200, {
+      message: "User Deleted Successfully",
+      user: deletedUser,
+    });
 
     console.log(users);
   }
@@ -177,35 +180,34 @@ const server = http.createServer((req, res) => {
     parthPart[3]
   ) {
     const userId = parseInt(parthPart[3]);
+    const users = await readUsersFromFile();
 
     const user = users.find((uid) => uid.id == userId);
 
     if (user) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(user));
+      sendJSON(res, 200, user);
     } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "User not found" }));
+      sendJSON(res, 404, { error: "User not found" });
     }
   }
 
+  //404 fallback API route
+  else if (parthPart[1] === "api") {
+    sendJSON(res, 404, { error: "API endpoint not found" });
+  }
   //other routes
   else if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" }); //describe the response
-    res.end("<h1>Home Page</h1>");
+    sendHTML(res, 200, "<h1>Home Page</h1>");
   } else if (req.method === "GET" && req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" }); //describe the response
-    res.end("<h1>About Page</h1>");
+    sendHTML(res, 200, "<h1>About Page</h1>");
   } else if (req.method === "GET" && req.url === "/contact") {
-    res.writeHead(200, { "Content-Type": "text/html" }); //describe the response
-    res.end("<h1>Contact Page</h1>");
+    sendHTML(res, 200, "<h1>Contact Page</h1>");
   } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>404 Page Not Found</h1>");
+    sendHTML(res, 404, "<h1>404 Page Not Found</h1>");
   }
 });
 
-const port = 3000; //creating the port
+const port = process.env.PORT || 3000; //creating the port
 
 //listening to the server
 server.listen(port, () => {
